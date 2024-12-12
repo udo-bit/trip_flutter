43f1372b9d852fa2d015572dcbf5c19c04299cc4
udo-bit <enote_gxl@163.com>
2024-12-12 11:32:00 +0800

    obx

--- lib/pages/travel_tab_page.dart
+++ lib/pages/travel_tab_page.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:trip_flutter/dao/travel_dao.dart';
+import 'package:get/get.dart';
+import 'package:trip_flutter/controller/travel_controller.dart';
 import 'package:trip_flutter/widget/loading_container.dart';
 import 'package:trip_flutter/widget/travel_item_widget.dart';
-
-import '../model/travel_tab_model.dart';
 
 class TravelTabPage extends StatefulWidget {
   final String groupChannelCode;
@@ -17,89 +16,62 @@ class TravelTabPage extends StatefulWidget {
 
 class _TravelTabPageState extends State<TravelTabPage>
     with AutomaticKeepAliveClientMixin {
-  final ScrollController _controller = ScrollController();
-
-  List<TravelItem> travelItems = [];
-  int pageIndex = 1;
-  bool _loading = true;
+  late TravelController travelController;
 
   get _gridView => MasonryGridView.count(
-        itemCount: travelItems.length,
-        controller: _controller,
+        itemCount: travelController.travelItems.length,
+        controller: travelController.scrollController,
         crossAxisCount: 2,
         itemBuilder: (BuildContext context, int index) {
-          return TravelItemWidget(item: travelItems[index]);
+          return TravelItemWidget(item: travelController.travelItems[index]);
         },
       );
 
+  get _obx => Obx(() => LoadingContainer(
+      isLoading: travelController.loading.value,
+      child: RefreshIndicator(
+          color: Colors.blue,
+          onRefresh: _handleRefresh,
+          child: MediaQuery.removePadding(
+            removeTop: true,
+            context: context,
+            child: _gridView,
+          ))));
+
+  // get _getx => GetX<TravelController>(
+  //       builder: (controller) {
+  //         return LoadingContainer(
+  //             isLoading: travelController.loading.value,
+  //             child: RefreshIndicator(
+  //                 color: Colors.blue,
+  //                 onRefresh: _handleRefresh,
+  //                 child: MediaQuery.removePadding(
+  //                   removeTop: true,
+  //                   context: context,
+  //                   child: _gridView,
+  //                 )));
+  //       },
+  //       init: travelController,
+  //     );
+
   @override
   void initState() {
-    _loadData();
     super.initState();
-    _controller.addListener(() {
-      if (_controller.position.pixels == _controller.position.maxScrollExtent) {
-        _loadData(loadMore: true);
-      }
-    });
+    travelController = Get.put(TravelController(widget.groupChannelCode),
+        tag: widget.groupChannelCode);
   }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return Scaffold(
-        body: LoadingContainer(
-            isLoading: _loading,
-            child: RefreshIndicator(
-                color: Colors.blue,
-                onRefresh: _loadData,
-                child: MediaQuery.removePadding(
-                  removeTop: true,
-                  context: context,
-                  child: _gridView,
-                ))));
-  }
-
-  Future<void> _loadData({loadMore = false}) async {
-    if (loadMore) {
-      pageIndex++;
-    } else {
-      pageIndex = 1;
-    }
-    try {
-      TravelTabModel? model =
-          await TravelDao.getTravels(widget.groupChannelCode, pageIndex, 10);
-      List<TravelItem> items = _filterItems(model?.list);
-      if (items.isEmpty) {
-        pageIndex--;
-      }
-      setState(() {
-        _loading = false;
-        if (loadMore) {
-          travelItems.addAll(items);
-        } else {
-          travelItems = items;
-        }
-      });
-    } catch (e) {
-      debugPrint(e.toString());
-      _loading = false;
-      if (loadMore) {
-        pageIndex--;
-      }
-    }
-  }
-
-  List<TravelItem> _filterItems(List<TravelItem>? list) {
-    if (list == null) return [];
-    List<TravelItem> filterItems = [];
-    for (var item in list) {
-      if (item.article != null) {
-        filterItems.add(item);
-      }
-    }
-    return filterItems;
+    return Scaffold(body: _obx);
   }
 
   @override
   bool get wantKeepAlive => true;
+
+  Future<void> _handleRefresh() async {
+    await travelController.loadData();
+    return;
+  }
 }
